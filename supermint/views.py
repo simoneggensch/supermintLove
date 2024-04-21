b95c0d0f51eedb2eee82e7df2568d409c116adb7
simoneggensch <simon.eggensch@gmail.com>
2024-04-21 09:39:31 +0200

    deleting views.py

--- supermint/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-import sqlite3
-from model.quiz.McCarthys import McCarthys
-from sqLite.utils import get_db_connection
-
-
-views = Blueprint("views", __name__)
-
